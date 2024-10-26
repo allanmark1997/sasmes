ed8c8a651638b43d444dc997229bedd85b4af728
Allan Mark G. Cabug-os <allanmark1997@gmail.com>
2024-10-27 00:18:12 +0800

    update

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -53,7 +53,7 @@ const submit = () => {
                     id="email"
                     v-model="form.email"
                     type="email"
-                    class="mt-1 block w-full"
+                    class="mt-1 block w-full text-gray-700"
                     required
                     autofocus
                     autocomplete="username"
